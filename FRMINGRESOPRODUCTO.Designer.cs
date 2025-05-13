e2a4e1c2e77a9052eaf31a9ccfde413df1e80e23
ErikJC25 <160562444+ErikJC25@users.noreply.github.com>
2025-05-12 20:12:18 -0500

    Diseño de FRMINGRESOPRODUCTO
    Se concreto el diseño del formulario FRMINGRESOPRODUCTO

--- FRMINGRESOPRODUCTO.Designer.cs
+++ FRMINGRESOPRODUCTO.Designer.cs
@@ -30,32 +30,312 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.TXT_codigo = new System.Windows.Forms.TextBox();
+            this.TXT_tipo = new System.Windows.Forms.TextBox();
+            this.TXT_color = new System.Windows.Forms.TextBox();
+            this.TXT_precio = new System.Windows.Forms.TextBox();
+            this.TXT_volumen = new System.Windows.Forms.TextBox();
+            this.TXT_marca = new System.Windows.Forms.TextBox();
+            this.TXT_proveedor = new System.Windows.Forms.TextBox();
+            this.TXT_material = new System.Windows.Forms.TextBox();
+            this.TXT_unit_venta = new System.Windows.Forms.TextBox();
+            this.TXT_stock = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.TXT_descript = new System.Windows.Forms.TextBox();
+            this.BTN_Ingresar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.BackColor = System.Drawing.Color.Plum;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(800, 100);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(12, 370);
+            this.panel2.BackColor = System.Drawing.Color.Plum;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 370);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 68);
+            this.panel2.Size = new System.Drawing.Size(800, 41);
             this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(172, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(463, 78);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingreso de Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tipo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Precio:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(22, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 22);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Color:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 22);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Volumen:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(258, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 22);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Proveedor:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(258, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 22);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Marca:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(258, 219);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 22);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Material:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(258, 304);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(148, 22);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Unidad de Venta:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(534, 132);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(112, 22);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "Descripción:";
+            // 
+            // TXT_codigo
+            // 
+            this.TXT_codigo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_codigo.Location = new System.Drawing.Point(102, 130);
+            this.TXT_codigo.Name = "TXT_codigo";
+            this.TXT_codigo.Size = new System.Drawing.Size(121, 26);
+            this.TXT_codigo.TabIndex = 13;
+            // 
+            // TXT_tipo
+            // 
+            this.TXT_tipo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_tipo.Location = new System.Drawing.Point(102, 173);
+            this.TXT_tipo.Name = "TXT_tipo";
+            this.TXT_tipo.Size = new System.Drawing.Size(121, 26);
+            this.TXT_tipo.TabIndex = 14;
+            // 
+            // TXT_color
+            // 
+            this.TXT_color.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_color.Location = new System.Drawing.Point(102, 217);
+            this.TXT_color.Name = "TXT_color";
+            this.TXT_color.Size = new System.Drawing.Size(121, 26);
+            this.TXT_color.TabIndex = 15;
+            // 
+            // TXT_precio
+            // 
+            this.TXT_precio.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_precio.Location = new System.Drawing.Point(102, 260);
+            this.TXT_precio.Name = "TXT_precio";
+            this.TXT_precio.Size = new System.Drawing.Size(106, 26);
+            this.TXT_precio.TabIndex = 16;
+            // 
+            // TXT_volumen
+            // 
+            this.TXT_volumen.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_volumen.Location = new System.Drawing.Point(113, 301);
+            this.TXT_volumen.Name = "TXT_volumen";
+            this.TXT_volumen.Size = new System.Drawing.Size(110, 26);
+            this.TXT_volumen.TabIndex = 17;
+            // 
+            // TXT_marca
+            // 
+            this.TXT_marca.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_marca.Location = new System.Drawing.Point(334, 130);
+            this.TXT_marca.Name = "TXT_marca";
+            this.TXT_marca.Size = new System.Drawing.Size(121, 26);
+            this.TXT_marca.TabIndex = 18;
+            // 
+            // TXT_proveedor
+            // 
+            this.TXT_proveedor.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_proveedor.Location = new System.Drawing.Point(364, 173);
+            this.TXT_proveedor.Name = "TXT_proveedor";
+            this.TXT_proveedor.Size = new System.Drawing.Size(121, 26);
+            this.TXT_proveedor.TabIndex = 19;
+            // 
+            // TXT_material
+            // 
+            this.TXT_material.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_material.Location = new System.Drawing.Point(350, 217);
+            this.TXT_material.Name = "TXT_material";
+            this.TXT_material.Size = new System.Drawing.Size(121, 26);
+            this.TXT_material.TabIndex = 20;
+            // 
+            // TXT_unit_venta
+            // 
+            this.TXT_unit_venta.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_unit_venta.Location = new System.Drawing.Point(412, 302);
+            this.TXT_unit_venta.Name = "TXT_unit_venta";
+            this.TXT_unit_venta.Size = new System.Drawing.Size(121, 26);
+            this.TXT_unit_venta.TabIndex = 21;
+            // 
+            // TXT_stock
+            // 
+            this.TXT_stock.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_stock.Location = new System.Drawing.Point(326, 260);
+            this.TXT_stock.Name = "TXT_stock";
+            this.TXT_stock.Size = new System.Drawing.Size(121, 26);
+            this.TXT_stock.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(258, 262);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(62, 22);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Stock:";
+            // 
+            // TXT_descript
+            // 
+            this.TXT_descript.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_descript.Location = new System.Drawing.Point(538, 157);
+            this.TXT_descript.Multiline = true;
+            this.TXT_descript.Name = "TXT_descript";
+            this.TXT_descript.Size = new System.Drawing.Size(250, 84);
+            this.TXT_descript.TabIndex = 25;
+            // 
+            // BTN_Ingresar
+            // 
+            this.BTN_Ingresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BTN_Ingresar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTN_Ingresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_Ingresar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_Ingresar.ForeColor = System.Drawing.Color.White;
+            this.BTN_Ingresar.Location = new System.Drawing.Point(631, 297);
+            this.BTN_Ingresar.Name = "BTN_Ingresar";
+            this.BTN_Ingresar.Size = new System.Drawing.Size(128, 35);
+            this.BTN_Ingresar.TabIndex = 26;
+            this.BTN_Ingresar.Text = "Ingresar";
+            this.BTN_Ingresar.UseVisualStyleBackColor = false;
             // 
             // FRMINGRESOPRODUCTO
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 411);
+            this.Controls.Add(this.BTN_Ingresar);
+            this.Controls.Add(this.TXT_descript);
+            this.Controls.Add(this.TXT_stock);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.TXT_unit_venta);
+            this.Controls.Add(this.TXT_material);
+            this.Controls.Add(this.TXT_proveedor);
+            this.Controls.Add(this.TXT_marca);
+            this.Controls.Add(this.TXT_volumen);
+            this.Controls.Add(this.TXT_precio);
+            this.Controls.Add(this.TXT_color);
+            this.Controls.Add(this.TXT_tipo);
+            this.Controls.Add(this.TXT_codigo);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "FRMINGRESOPRODUCTO";
             this.Text = "FRMINGRESOPRODUCTO";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -63,5 +343,29 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox TXT_codigo;
+        private System.Windows.Forms.TextBox TXT_tipo;
+        private System.Windows.Forms.TextBox TXT_color;
+        private System.Windows.Forms.TextBox TXT_precio;
+        private System.Windows.Forms.TextBox TXT_volumen;
+        private System.Windows.Forms.TextBox TXT_marca;
+        private System.Windows.Forms.TextBox TXT_proveedor;
+        private System.Windows.Forms.TextBox TXT_material;
+        private System.Windows.Forms.TextBox TXT_unit_venta;
+        private System.Windows.Forms.TextBox TXT_stock;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox TXT_descript;
+        private System.Windows.Forms.Button BTN_Ingresar;
     }
 }
