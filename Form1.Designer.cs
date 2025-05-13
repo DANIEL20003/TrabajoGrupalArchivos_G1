dac49f36230e217843c8debc219f55c6ec495d70
ErikJC25 <160562444+ErikJC25@users.noreply.github.com>
2025-05-13 11:24:21 -0500

    Sutil

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -99,7 +99,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(703, 489);
+            this.label1.Location = new System.Drawing.Point(19, 462);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 24);
             this.label1.TabIndex = 7;
